104746de70d905935809424bbc8795ecbcaa75d1
Ayush Malik <malikayush580@gmail.com>
2024-07-04 08:50:06 +0530

    Pushing all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import  styled, {ThemeProvider} from "styled-components"
+import  {darkTheme} from "./utils/Themes.js"
+import Navbar from './components/navbar';
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero.js";
+import Skills from "./components/sections/skills"
+import Projects from "./components/sections/projects.js";
+import Footer from "./components/sections/footer.js";
 
+
+const Body = styled.div`
+background-color: ${({theme}) => theme.bg};
+width: 100%;
+height:90vh;
+overflow-x:hidden;
+position:relative;
+`
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ThemeProvider theme={darkTheme}>
+   <BrowserRouter>
+   <Navbar/>
+   <Body>
+    <Hero/>
+    <Skills/>
+    <Projects/>
+    <Footer/>
+   </Body>
+   </BrowserRouter>
+  </ThemeProvider>
 }
 
 export default App;
